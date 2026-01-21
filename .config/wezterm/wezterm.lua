151b63ee667bb34f373ee03d653a54c702516ecb
aamini <amini5454@gmail.com>
2026-01-20 20:15:20 -0500

    Format

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,33 +1,38 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.keys = {
-  -- paste from the clipboard
-  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+	-- paste from the clipboard
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 
-  -- paste from the primary selection
-  { key = 'v', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+	-- paste from the primary selection
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
 }
 
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font("JetBrains Mono")
 
-config.default_domain = 'WSL:Ubuntu'
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 20
 
--- Enable tab bar and show clock in right status
-wezterm.on('update-right-status', function(window, pane)
-  local date = wezterm.strftime '%H:%M  %b %d'
-  window:set_right_status(wezterm.format {
-    { Foreground = { Color = '#7aa2f7' } },
-    { Text = date },
-  })
+-- Enable tab bar to show clock in right status
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.show_tabs_in_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+wezterm.on("update-right-status", function(window)
+	local date = wezterm.strftime("%H:%M  %b %d")
+	window:set_right_status(wezterm.format({
+		{ Foreground = { Color = "#7aa2f7" } },
+		{ Text = date },
+	}))
 end)
 
 return config
