18393c7c7a8b82aeba9cd2bd263a124613d10eb0
aamini <amini5454@gmail.com>
2026-01-20 20:27:56 -0500

    Remove wezterm time bar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,17 +22,6 @@ config.font = wezterm.font("JetBrains Mono")
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 20
 
--- Enable tab bar to show clock in right status
-config.enable_tab_bar = true
-config.use_fancy_tab_bar = false
-config.show_tabs_in_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = false
-wezterm.on("update-right-status", function(window)
-	local date = wezterm.strftime("%H:%M  %b %d")
-	window:set_right_status(wezterm.format({
-		{ Foreground = { Color = "#7aa2f7" } },
-		{ Text = date },
-	}))
-end)
+config.enable_tab_bar = false
 
 return config
