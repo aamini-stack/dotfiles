c4b4200c8cb6baa9250520b6fea8938524ac4f06
Aria Amini <amini5454@gmail.com>
2026-02-25 14:23:16 -0500

    Minor fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,20 +2,10 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname -- "$0"; )" && pwd; )"
-REPO_DIR="$( cd "$SCRIPT_DIR/.." && pwd; )"
-NIX_SH="$HOME/.nix-profile/etc/profile.d/nix.sh"
-
 if ! command -v nix >/dev/null 2>&1; then
-  if [ -r "$NIX_SH" ]; then
-    . "$NIX_SH"
-  fi
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+  . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
-
-if ! command -v nix >/dev/null 2>&1; then
-  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon --no-modify-profile
-  . "$NIX_SH"
-fi
-
 cd "$REPO_DIR"
 nix profile add ".#default" --extra-experimental-features nix-command --extra-experimental-features flakes
 
@@ -28,5 +18,5 @@ if [[ $STATUS = 'L' ]]; then
   echo "User has no password. Please set one as it is required to change the default shell to zsh"
   sudo passwd $USER 
 fi
-echo "Changing Shell"
-chsh -s "$(which zsh)" "$USER"
+echo "Changing Default Shell (Please Enter Password)"
+sudo chsh -s "$(which zsh)" "$USER"
