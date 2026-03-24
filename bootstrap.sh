4b7ccff6e9f927572c460ceb0916d4f7a18c0042
Aria Amini <amini5454@gmail.com>
2026-03-24 05:04:17 -0400

    Revert "login"
    This reverts commit f47ee70689f767285fd7f545e9bbb37bcd409796.

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,50 +12,6 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1
 }
 
-resolve_login_shell() {
-  local user_name login_shell
-
-  user_name="${USER:-$(id -un)}"
-
-  if command_exists getent; then
-    login_shell="$(getent passwd "$user_name" | cut -d: -f7)"
-  elif command_exists dscl; then
-    login_shell="$(dscl . -read "/Users/$user_name" UserShell 2>/dev/null | awk '{print $2}')"
-  else
-    login_shell="${SHELL:-}"
-  fi
-
-  printf '%s\n' "$login_shell"
-}
-
-maybe_start_login_shell() {
-  local current_shell login_shell
-
-  if [[ ! -t 0 || ! -t 1 ]]; then
-    return
-  fi
-
-  login_shell="$(resolve_login_shell)"
-  current_shell="${SHELL:-}"
-
-  if [[ -z "$login_shell" || ! -x "$login_shell" || "$login_shell" == "$current_shell" ]]; then
-    return
-  fi
-
-  case "${DOTFILES_AUTO_LOGIN_SHELL:-1}" in
-    0|false|FALSE|no|NO)
-      log "Default shell is now $login_shell"
-      log "Start it without reconnecting: exec \"$login_shell\" -l"
-      return
-      ;;
-  esac
-
-  log "Default shell is now $login_shell"
-  log "Starting a login shell now. Exit once to return to your previous shell."
-  export SHELL="$login_shell"
-  exec "$login_shell" -l
-}
-
 main() {
   if ! command_exists git; then
     log "git is required to bootstrap dotfiles"
@@ -74,7 +30,6 @@ main() {
   fi
 
   bash "$INSTALL_DIR/scripts/install.sh"
-  maybe_start_login_shell
 }
 
 main "$@"
