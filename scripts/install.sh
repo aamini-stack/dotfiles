567663b797033c808e1dd8a59f8d5588d242261e
Aria Amini <amini5454@gmail.com>
2026-04-01 17:18:53 -0400

    install

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,25 +2,28 @@
 set -euo pipefail
 cd $HOME
 
-sudo apt install zsh
+# stow
+sudo apt install stow -y
+stow -v --dotfiles --adopt .
+
+sudo apt install zsh -y
+if getent passwd "$USER" >/dev/null; then
+    sudo passwd "$USER"
+else
+    echo "User $USER does not exist – nothing to do."
+fi
+chsh "$(which zsh)" $USER
 
 # nix
-mkdir -p .config/nix
 [[ ! -e .config/nix/nix.conf ]] && cp dotfiles/dot-config/nix/nix.conf .config/nix/nix.conf
 if ! command -v nix &> /dev/null; then
   sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+  . /home/aamini.linux/.nix-profile/etc/profile.d/nix.sh
 fi
-. /home/aamini.linux/.nix-profile/etc/profile.d/nix.sh
 
 # mise
 export PATH="$PATH:$HOME/.local/bin"
-mkdir -p .config/mise
-[[ ! -e .config/mise/config.toml ]] && cp ~/dotfiles/dot-config/mise/config.toml .config/mise/config.toml
 curl https://mise.run | sh
 mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i --silent
 
-[[ ! -e .zshrc ]] && cp dotfiles/dot-zshrc .zshrc
-
-cd dotfiles
-stow -v --dotfiles --adopt .
