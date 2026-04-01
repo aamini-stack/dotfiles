2802bf79e095feb73a6344900edc2bcd2da779ea
Aria Amini <amini5454@gmail.com>
2026-04-01 14:35:26 -0400

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,14 @@ sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daem
 . /home/aamini.linux/.nix-profile/etc/profile.d/nix.sh
 
 # mise
-mkdir -p ~/.config/mise
-ln -s ~/dotfiles/dot-config/mise/config.toml ~/.config/mise/config.toml
-mkdir -p ~/.config/nix
-ln -s ~/dotfiles/dot-config/nix/nix.conf ~/.config/nix/nix.conf
+cd $HOME 
+mkdir -p .config/mise
+cp ~/dotfiles/dot-config/mise/config.toml .config/mise/config.toml
+mkdir -p .config/nix
+cp dotfiles/dot-config/nix/nix.conf .config/nix/nix.conf
 curl https://mise.run | sh
 mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i 
 
-rm -r ~/.config/mise
-rm -r ~/.config/nix
-stow -v --dir="$HOME/dotfiles" --target "$HOME"
+cd dotfiles
+stow -v --dotfiles --adopt .
