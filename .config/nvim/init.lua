afd4c1549f7e9ca1299d5b3326b95a1d6c3e69b1
Aria Amini <amini5454@gmail.com>
2026-04-20 21:32:07 -0400

    add copy/paste

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,12 +76,6 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
 vim.o.breakindent = true
 
 -- Save undo history
@@ -201,6 +195,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard:append 'unnamedplus'
+
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = { ['+'] = require('vim.ui.clipboard.osc52').copy '+', [''] = require('vim.ui.clipboard.osc52').copy '' },
+    paste = { ['+'] = require('vim.ui.clipboard.osc52').paste '+', [''] = require('vim.ui.clipboard.osc52').paste '' },
+  }
+end)
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more
 --    info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
