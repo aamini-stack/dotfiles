28385f1ac8fd1fcdf1503558b79c3fa47ea43124
Aria Amini <amini5454@gmail.com>
2026-04-29 05:15:10 -0400

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,7 +17,7 @@ export PATH="$PATH:$HOME/.local/bin"
 curl https://mise.run | sh
 mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i
-mise activate
+eval "$(mise activate bash)"
 
 stow --restow -v --target="$HOME" --dotfiles .
 sudo chsh -s "$(which zsh)" $USER
