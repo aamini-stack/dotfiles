7028648e51901c2473d81b07a1fdbe7301e1202a
Aria Amini <amini5454@gmail.com>
2026-04-29 06:42:52 -0400

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,29 +8,28 @@ if ! command -v nix &> /dev/null; then
   . $HOME/.nix-profile/etc/profile.d/nix.sh
 fi
 
-# mise
-mkdir -p .config/mise
-cp $HOME/dotfiles/.config/mise/config.toml $HOME/.config/mise/config.toml
-mkdir -p .config/nix
-cp $HOME/dotfiles/.config/nix/nix.conf $HOME/.config/nix/nix.conf
-export PATH="$PATH:$HOME/.local/bin"
-curl https://mise.run | sh
-mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
-mise i
-eval "$(mise activate bash)"
-rm -r $HOME/.config/nix 
-rm -r $HOME/.config/mise
-rm .zshrc
-stow --restow -v --target="$HOME" .
-sudo chsh -s "$(which zsh)" $USER
-
 # tmux plugin manager (tpm)
 if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm.git ~/.config/tmux/plugins/tpm
 fi
 
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh ./get-docker.sh
-# sudo usermod -aG docker $USER
-# sudo reboot
+# mise
+mkdir -p .config/mise
+cp -n $HOME/dotfiles/.config/mise/config.toml $HOME/.config/mise/config.toml
+mkdir -p .config/nix
+cp -n $HOME/dotfiles/.config/nix/nix.conf $HOME/.config/nix/nix.conf
+export PATH="$PATH:$HOME/.local/bin"
+curl https://mise.run | sh
+mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
+mise i
+eval "$(mise activate bash)"
+rm -r $HOME/.config/nix
+rm -r $HOME/.config/mise
+if [ -d "$HOME/.zshrc" ]; then
+  rm "$HOME/.zshrc"
+fi
+cd dotfiles
+mise trust
+stow --restow -v --target="$HOME" .
+sudo chsh -s "$(which zsh)" $USER
 
