6c469fa29b6ecca7d20fbd6c8aa05c3406f240cc
Aria Amini <amini5454@gmail.com>
2026-04-30 04:33:52 -0400

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,21 +15,23 @@ fi
 
 # mise
 mkdir -p .config/mise
-cp -n $HOME/dotfiles/.config/mise/config.toml $HOME/.config/mise/config.toml
+cp -u $HOME/dotfiles/.config/mise/config.toml $HOME/.config/mise/config.toml
 mkdir -p .config/nix
-cp -n $HOME/dotfiles/.config/nix/nix.conf $HOME/.config/nix/nix.conf
-export PATH="$PATH:$HOME/.local/bin"
-curl https://mise.run | sh
+cp -u $HOME/dotfiles/.config/nix/nix.conf $HOME/.config/nix/nix.conf
+
+sudo add-apt-repository -y ppa:jdxcode/mise
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y mise
+echo 'export PATH="$PATH:~/.local/bin"' >> ~/.bashrc
+echo 'eval "$(mise activate bash)"' >> ~/.bashrc
 mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i
-eval "$(mise activate bash)"
 rm -r $HOME/.config/nix
 rm -r $HOME/.config/mise
-if [ -d "$HOME/.zshrc" ]; then
-  rm "$HOME/.zshrc"
-fi
 cd dotfiles
 mise trust
 stow --restow -v --target="$HOME" .
+command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s "$(which zsh)" $USER
 
