02d15cd0c9d3b1dd457c962e76f53a0d43b2598b
Aria Amini <amini5454@gmail.com>
2026-04-30 04:52:24 -0400

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,25 +13,17 @@ if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 fi
 
-# mise
-mkdir -p .config/mise
-cp -u $HOME/dotfiles/.config/mise/config.toml $HOME/.config/mise/config.toml
-mkdir -p .config/nix
-cp -u $HOME/dotfiles/.config/nix/nix.conf $HOME/.config/nix/nix.conf
+curl https://mise.run | sh
+sudo apt install stow -y
+stow --restow -v --target="$HOME" .
 
-sudo add-apt-repository -y ppa:jdxcode/mise
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y mise
 echo 'export PATH="$PATH:~/.local/bin"' >> ~/.bashrc
 echo 'eval "$(mise activate bash)"' >> ~/.bashrc
+
 mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i
-rm -r $HOME/.config/nix
-rm -r $HOME/.config/mise
-cd dotfiles
-mise trust
-stow --restow -v --target="$HOME" .
+
+# zsh
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s "$(which zsh)" $USER
 
