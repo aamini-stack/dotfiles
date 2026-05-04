777b819d5c273aa702748429de7b2e07fb7edaed
Aria Amini <amini5454@gmail.com>
2026-05-04 00:17:23 -0400

    fix install: large windows refactor

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,32 +2,45 @@
 set -euo pipefail
 cd $HOME
 
-# nix
-if ! command -v nix &> /dev/null; then
-  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
-  . $HOME/.nix-profile/etc/profile.d/nix.sh
+# clone: dotfiles
+if [ ! -d "$HOME/dotfiles" ]; then
+  git clone "https://github.com/aamini-stack/dotfiles.git" "$HOME/dotfiles"
 fi
 
-# tmux plugin manager (tpm)
-if [ ! -d "$HOME/.config/tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
-fi
+# wsl
+# sudo cp dotfiles/misc/wsl.conf /etc/wsl.conf
 
-curl https://mise.run | sh
+# stow
+sudo apt update -y
 sudo apt install stow -y
-rm .zshrc
+if [ -f "$HOME/.zshrc" ]; then
+  rm "$HOME/.zshrc"
+fi
 cd dotfiles
 stow --restow -v --target="$HOME" .
 cd $HOME
 
-export PATH="$PATH:~/.local/bin"
-echo 'export PATH="$PATH:~/.local/bin"' >> ~/.bashrc
-echo "eval \"\$(/home/aamini.linux/.local/bin/mise activate bash)\"" >> ~/.bashrc
+# nix
+if ! command -v nix &> /dev/null; then
+  sudo sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+  . "$HOME/.nix-profile/etc/profile.d/nix.sh"
+fi
 
-mise plugin list | grep -q "^nix" || mise plugin install nix https://github.com/jbadeau/mise-nix.git
+# mise
+export PATH="$PATH:$HOME/.local/bin"
+echo "eval \"\$($HOME/.local/bin/mise activate bash)\"" >> ~/.bashrc
+source .bashrc
+curl https://mise.run | sh
+mise plugin install nix https://github.com/jbadeau/mise-nix.git
 mise i
 
 # zsh
-command -v zsh | sudo tee -a /etc/shells
-sudo chsh -s "$(which zsh)" $USER
+sudo usermod -s "$(which zsh)" $USER
+# command -v zsh | sudo tee -a /etc/shells
+# sudo chsh -s "$(which zsh)" $USER
+
+# tpm (tmux plugin manager)
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone "https://github.com/tmux-plugins/tpm.git" "$HOME/.tmux/plugins/tpm"
+fi
 
