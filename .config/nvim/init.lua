783f092647997d6e3a8338e76eedace97cd6cb01
Aria Amini <amini5454@gmail.com>
2026-07-21 17:06:09 -0400

    add autoread

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -97,6 +97,10 @@ vim.o.linebreak = true
 -- Save undo history
 vim.o.undofile = true
 
+-- Reload buffers changed outside of Neovim (e.g. by another editor or agent)
+-- See `:help 'autoread'`
+vim.o.autoread = true
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
