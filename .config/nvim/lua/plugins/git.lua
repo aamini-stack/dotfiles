b42e15550b21ca15448883cd3c4c34d4491f9352
Aria Amini <amini5454@gmail.com>
2026-08-04 04:52:29 -0400

    allow collapsing sidebar in diff

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -5,6 +5,11 @@ return {
     dependencies = { 'MunifTanjim/nui.nvim' },
     config = function()
       require('hunk').setup {
+        ui = {
+          tree = {
+            use_float = true,
+          },
+        },
         hooks = {
           on_tree_mount = function(context)
             -- Hunk does not expose its preview action yet (julienvincent/hunk.nvim#36),
